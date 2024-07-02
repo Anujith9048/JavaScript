c65dec2bf001435b784c852bc237c338573f16ca
Anujith9048 <anujith.s@techversantinfotech.com>
2024-07-02 11:02:20 +0530

    edited

--- 52_Ascii/script/script.js
+++ 52_Ascii/script/script.js
@@ -2,6 +2,7 @@ function findState() {
     var str =document.getElementById("str").value;
     var str = str.trim();
     let total = 0;
+    
     for (let i = 0; i < str.length; i++) {
         total += str.charCodeAt(i);
     }
